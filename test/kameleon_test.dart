cf42042071c96883050677af668af4f9665a44cf
~ FelipeC <fcostax02@gmail.com>
2020-05-18 09:39:30 -0300

    Removing default tests

---
+++ test/kameleon_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  test('adds one to input values', () {
+    final mRaisedButton = RaisedButton();
+    // TODO make some tests here ~
+  });
+}
